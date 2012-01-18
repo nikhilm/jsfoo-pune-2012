77d0e9397110958f160f94425359da46956f93d4
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-01-18 22:15:23 +0530

    Demonstrate NODE_DEFINE_CONSTANT

--- primitives/primitives.cc
+++ primitives/primitives.cc
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <v8.h>
 #include <node.h>
 #include <node_version.h>
@@ -5,11 +6,12 @@ using namespace v8;
 
 extern "C" {
     static void Init(Handle<Object> target) {
-        target->Set(String::New("answer"),
-                    Integer::New(42));
+        target->Set(String::NewSymbol("pi"),
+                    Number::New(M_PI));
 
-        target->Set(String::New("node_version"),
-                    String::New(NODE_VERSION_STRING));
+        NODE_DEFINE_CONSTANT(target, NODE_MINOR_VERSION);
+
+        target->Set(String::New("name"), String::New("Nikhil"));
     }
     NODE_MODULE(primitives, Init)
 }
